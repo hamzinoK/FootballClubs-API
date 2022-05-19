3b5d927898a5737d359337768798297172d69e31
Hamza Khan <hamza.kh28@gmail.com>
2022-05-19 15:21:24 +0100

    derived query yet to do

--- footballclub/src/main/java/com/bnta/footballclub/controllers/FootballerController.java
+++ footballclub/src/main/java/com/bnta/footballclub/controllers/FootballerController.java
@@ -2,14 +2,14 @@ package com.bnta.footballclub.controllers;
 
 import com.bnta.footballclub.models.Footballer;
 import com.bnta.footballclub.repositories.FootballerRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("footballers")
@@ -18,9 +18,28 @@ public class FootballerController {
 
     @Autowired
     FootballerRepository footballerRepository;
-    // get/index
+    // GET/INDEX route
     @GetMapping
     public ResponseEntity<List<Footballer>> getAllFootballers(){
         return new ResponseEntity<>(footballerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping //localhost:8080/footballers?playerRating=86
+    public ResponseEntity<List<Footballer>> getAllFootballersWithAPlayerRatingOf86(@Request) {
+        
+    }
+
+    // SHOW route
+
+    @GetMapping(value = "/{id}") //localhost:8080/footballers/1
+    public ResponseEntity<Optional<Footballer>> getFootballer(@PathVariable Long id) {
+        return new ResponseEntity<>(footballerRepository.findById(id), HttpStatus.OK);
+    }
+
+    // POST route
+    @PostMapping // POST localhost:8080/pets
+    public ResponseEntity<Footballer> createFootballer(@RequestBody Footballer newFootballer) {
+        footballerRepository.save(newFootballer);
+        return new ResponseEntity<>(newFootballer, HttpStatus.CREATED);
     }
 }
--- footballclub/src/main/java/com/bnta/footballclub/repositories/FootballerRepository.java
+++ footballclub/src/main/java/com/bnta/footballclub/repositories/FootballerRepository.java
@@ -3,5 +3,11 @@ package com.bnta.footballclub.repositories;
 import com.bnta.footballclub.models.Footballer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FootballerRepository extends JpaRepository<Footballer, Long> {
+
+// Derived Query to return all players of 'playerRating' over 86
+    List<Footballer> findFootballerByPlayerRating(int playerRating);
+
 }
